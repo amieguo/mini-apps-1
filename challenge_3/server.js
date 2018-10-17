d45bf116732aad09990b079f15a9c07db7fbd470
Amie Guo <amieguo@gmail.com>
2018-10-17 12:31:52 -0700

    setting up

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,18 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+var bodyParser = require('body-parser');
+var path = require('path');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static('./public'));
 
 
+app.post('/test', (req, res) => {
+    res.send(console.log('hi'))
+})
+
+
+app.listen(port, () => console.log(`listening on port ${port}`));

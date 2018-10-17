80c19f1997cba0a888c237b555a256a75df88efe
Amie Guo <amieguo@gmail.com>
2018-10-16 20:38:20 -0700

    completed

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,18 @@
+// const $ = require('jQuery')
 
+$(document).ready(function() {
+    $('form').submit(function(event) {
+        event.preventDefault();
+        let text = event.target[0].value;
 
+        $.ajax('/sales_report', {
+            method: 'POST', 
+            contentType: 'application/json',
+            data: JSON.stringify({text}),
+            success: (data) => {
+                $('body').html('')
+                $('body').append(data);
+            }})
+    })
+    console.log('testing');
+})
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,41 +1,41 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const $ = require('jquery')
+// const $ = require('jquery')
 var bodyParser = require('body-parser');
 var path = require('path');
+var fs = require('fs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); 
+
 
 
 app.use(express.static('./client'))
 
-// var items = document.getElementById('app').value
-// document.getElementById('btn').addEventListener('click', clicked);;
-// var fields = Object.keys(json[0]);
-// let csv = items.map(row => fields.map(fieldName => JSON.stringify(row[fieldName])).join(','))
-// csv = csv.join('\r\n')
-// console.log(csv)
 
 
 app.post('/sales_report', (req, res) => { 
-    // if (err) {
-    //     res.status(500).send(); 
-    // } else {
-        // res.send(req.body);
-        res.send(req.body)
-        // res.send( req.body.sometext)
-
-
-        // "hello",
-        
-    
+    var text = req.body.sometext;
+    console.log(req.body)
+    var par = JSON.parse(text);
+    var fields = Object.keys(par);
+    // res.send(req.data)
+    // var smth = (typeof text)
+    let csv = fields.map(row => fields.map(fieldName => JSON.stringify(row[fieldName])).join(','))
+    csv.unshift(fields.join(','))
+    csv = csv.join('\r\n')
+    // // fs.writeFile 
+    // console.log(req.data);
+    // res.send(JSON.stringify(req.data));
+    res.send(csv);
     
 });
 
 
 
-// $.ajax({method: 'POST', url: '/sales_report', })
+
 
 
 
